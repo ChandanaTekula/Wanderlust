14aad81e895346e2bcaf1040818e9b25be9dc61e
Tekula-Chandana <tekula.chandana1419@gmail.com>
2025-07-18 01:19:30 +0530

    Add Project Files

---
+++ routes/user.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware");
+const userController = require("../controllers/user");
+
+//Signup
+router
+  .route("/signup")
+  .get(userController.renderSignupForm)
+  .post(wrapAsync(userController.signUp));
+
+//Login
+router
+  .route("/login")
+  .get(userController.renderLoginForm)
+  .post(
+    saveRedirectUrl,
+    passport.authenticate("local", {
+      failureRedirect: "/login",
+      failureFlash: true,
+    }),
+    userController.login
+  );
+
+//Logout
+router.get("/logout", userController.logout);
+module.exports = router;
